3a3410b6c62631feed77db50d1cd6c8d1942ad41
1VolumeZeZ1 <131445435+1VolumeZeZ1@users.noreply.github.com>
2023-04-23 12:45:07 +0300

    Add files via upload

---
+++ Task3/Task3/Task3.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    float   ch1, ch2, res;
+    char    sim;
+    cout << "Two Numbers: \n";
+    cin >> ch1 >> ch2;
+    cout << "Action: \n";
+    cin >> sim;
+    switch (sim)
+    {
+    case '*':
+        res = ch1 * ch2;
+        break;
+    case '/':
+        res = ch1 / ch2;
+        break;
+    case '-':
+        res = ch1 - ch2;
+        break;
+    case '+':
+        res = ch1 + ch2;
+        break;
+    default:
+        cout << "ERROR...\n";
+        system("pause");
+        return 0;
+    }
+    cout << ch1 << sim << ch2 << "=" << res << endl;
+    system("pause");
+    return 0;
+}
